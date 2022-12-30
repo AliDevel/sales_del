2396d17659b39252064c555318f3d93f71974075
AliDevel <alimerdanrahimov@gmail.com>
2022-12-30 11:47:45 +0000

    new

--- sales_order/delete_sales_order/delete.py
+++ sales_order/delete_sales_order/delete.py
@@ -9,11 +9,13 @@ class SalesOrderDelete:
         with open("/home/frappe/frappe-bench/apps/sales_order/sales_order/delete_sales_order/sales.csv", 'r') as file:
             sales_order = csv.reader(file)           
             for id in sales_order:
-                frappe.msgprint(id[0])
-                sal= frappe.get_doc('Sales Order',id[0])
-                sal.cancel()
-                frappe.msgprint(sal.status)
-                frappe.db.commit()
+                name= frappe.db.get_value('Sales Order',id[0],'name')
+                if str(name) != 'None':
+                    sal= frappe.get_doc('Sales Order',id[0])
+                    frappe.msgprint(str(name))
+                    sal.cancel()
+                    frappe.msgprint(sal.status)
+                    frappe.db.commit()
         
        
                    
