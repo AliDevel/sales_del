7fa8de077afb9af2d74c76e672df0198e2fca31e
AliDevel <alimerdanrahimov@gmail.com>
2023-01-01 14:40:14 +0000

    Quotation cancel

--- sales_order/delete_sales_order/delete.py
+++ sales_order/delete_sales_order/delete.py
@@ -9,9 +9,9 @@ class SalesOrderDelete:
         with open("/home/frappe/frappe-bench/apps/sales_order/sales_order/delete_sales_order/sales.csv", 'r') as file:
             sales_order = csv.reader(file)           
             for id in sales_order:
-                name= frappe.db.get_value('Sales Order',id[0],'name')
+                name= frappe.db.get_value('Quotation',id[0],'name')
                 if str(name) != 'None':
-                    sal= frappe.get_doc('Sales Order',id[0])
+                    sal= frappe.get_doc('Quotation',id[0])
                     frappe.msgprint(str(name))
                     sal.cancel()
                     frappe.msgprint(sal.status)
